599814063141a53940ca11b90bf3716bb27d3d03
ayriela <53126621+ayriela@users.noreply.github.com>
2019-10-15 15:31:14 -0500

    commented correctly

--- database.sql
+++ database.sql
@@ -1,43 +1,43 @@
 
-How do you get all users from Chicago?
---select * from "accounts" where "city"='chicago'
+--How do you get all users from Chicago?
+select * from "accounts" where "city"='chicago'
 
-How do you get all users with usernames that contain the letter a?
---select * from "accounts" where "username" ilike '%a%'
+--How do you get all users with usernames that contain the letter a?
+select * from "accounts" where "username" ilike '%a%'
 
-The bank is giving a new customer bonus! How do you update all records with an account balance of 0.00 and a transactions_attempted of 0? Give them a new account balance of 10.00.
---update "accounts" set "account_balance"=10.00 where "transactions_attempted"=0 and "account_balance"=0
+--The bank is giving a new customer bonus! How do you update all records with an account balance of 0.00 and a transactions_attempted of 0? Give them a new account balance of 10.00.
+update "accounts" set "account_balance"=10.00 where "transactions_attempted"=0 and "account_balance"=0
 
-How do you select all users that have attempted 9 or more transactions?
---select * from "accounts" where "transactions_attempted">9
+--How do you select all users that have attempted 9 or more transactions?
+select * from "accounts" where "transactions_attempted">9
 
-How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
---select "username", "account_balance" from "accounts" order by "account_balance" desc limit 3
+--How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
+select "username", "account_balance" from "accounts" order by "account_balance" desc limit 3
 
-How do you get the username and account balance of the 3 users with the lowest balances, sorted lowest to highest balance?
---select "username", "account_balance" from "accounts" order by "account_balance" asc limit 3
+--How do you get the username and account balance of the 3 users with the lowest balances, sorted lowest to highest balance?
+select "username", "account_balance" from "accounts" order by "account_balance" asc limit 3
 
-How do you get all users with account balances that are more than $100?
---select * from "accounts" where "account_balance">100
+--How do you get all users with account balances that are more than $100?
+select * from "accounts" where "account_balance">100
 
-How do you add a new account?
---insert into "accounts" ("username","city","transactions_completed", "transactions_attempted", "account_balance") values('ayriel','st. paul','0','0','100.00')
+--How do you add a new account?
+insert into "accounts" ("username","city","transactions_completed", "transactions_attempted", "account_balance") values('ayriel','st. paul','0','0','100.00')
 
-The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
---delete from "accounts" where ("city"='miami' or "city"='phoenix') AND "transactions_completed"<5
-
-
-Anthony moved to Santa Fe.
---update "accounts" set "city"='santa fe' where "username"='anthony'
+--The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+delete from "accounts" where ("city"='miami' or "city"='phoenix') AND "transactions_completed"<5
 
 
-Grace closed her account.
---delete from "accounts" where "username"='grace'
+--Anthony moved to Santa Fe.
+update "accounts" set "city"='santa fe' where "username"='anthony'
 
-Travis made a withdrawl of $20,000. What's their new balance? NOTE: Research RETURNING
---update "accounts" set "account_balance" = "account_balance" - 20000
+
+--Grace closed her account.
+delete from "accounts" where "username"='grace'
+
+--Travis made a withdrawl of $20,000. What's their new balance? NOTE: Research RETURNING
+update "accounts" set "account_balance" = "account_balance" - 20000
   where "username"='travis'
   returning "account_balance";
 
-The Bank needs to track last names. NOTE: Research ALTER TABLE https://www.postgresql.org/docs/10/static/sql-altertable.html
---alter table "accounts" add column "lastname" varchar(80)
+--The Bank needs to track last names. NOTE: Research ALTER TABLE https://www.postgresql.org/docs/10/static/sql-altertable.html
+alter table "accounts" add column "lastname" varchar(80)
